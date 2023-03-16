e06afccecff44398f5fe4cac8dfc1acf59a62e75
Emon Chowdhury <90343790+EmonChow@users.noreply.github.com>
2023-03-16 19:32:44 +0600

    update product urls

--- src/product/urls.py
+++ src/product/urls.py
@@ -4,6 +4,6 @@ from product import views
 urlpatterns = [
     path('', views.home_view, name='home'),
     path('search', views.search_view, name='search'),
-    path('add', views.add_product_view, name='add'),
+    path('add/', views.add_product_view, name='add'),
     path('edit/<int:pk>', views.edit_product, name='edit')
 ]
